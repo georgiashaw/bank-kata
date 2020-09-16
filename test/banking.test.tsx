90dd6ba4fc12a8ecafdb77c5e3e4277d822af7bd
Georgia <georgia@cranbrookw4.com>
2020-09-16 13:12:12 +0100

    --wip-- [skip ci]

---
+++ test/banking.test.tsx
@@ -0,0 +1,20 @@
+// Given a client makes a deposit of 1000 on 10-01-2012
+// And a deposit of 2000 on 13-01-2012
+// And a withdrawal of 500 on 14-01-2012
+// When they print their bank statement
+// Then they would see:
+
+// Date       || Amount || Balance
+// 14/01/2012 || -500   || 2500
+// 13/01/2012 || 2000   || 3000
+// 10/01/2012 || 1000   || 1000
+
+
+it('it should allow a user to perform banking operations', () => {
+    const userBankAccount = new BankAccount()
+    const printer = jest.fn()
+expect(printer).toHaveBeenCalledWith(`Date       || Amount || Balance
+14/01/2012 || -500   || 2500
+13/01/2012 || 2000   || 3000
+10/01/2012 || 1000   || 1000`)
+})
